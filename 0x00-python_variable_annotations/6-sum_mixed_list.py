d8014d19a2460590ea5f505e693347b1d0e67ed9
deenakamal <deenah.kamal@gmail.com>
2024-07-05 02:31:20 +0000

    6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """ return sum of mixed list """
+    return sum(mxd_lst)
