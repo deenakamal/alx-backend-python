7b7ea26c5ccebf626d8de4e9c8cb90bd71dea4f3
deenakamal <deenah.kamal@gmail.com>
2024-07-08 16:28:43 -0700

    3, 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module"""
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Executes task_wait_random"""
+    list_waited = []
+    for i in range(n):
+        wait_time = await task_wait_random(max_delay)
+        list_waited.append(wait_time)
+
+    return sorted(list_waited)
