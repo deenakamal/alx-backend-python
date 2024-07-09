b532c97ce637f561c50663284afe996c5577e6d0
deenakamal <deenah.kamal@gmail.com>
2024-07-09 12:44:34 -0700

    1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' Module '''
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''coroutine will collect 10 random numbers '''
+    result = []
+    async for i in async_generator():
+        result.append(i)
+
+    return result
