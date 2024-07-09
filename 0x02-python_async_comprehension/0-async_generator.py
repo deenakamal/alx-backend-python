41e258333d710200596dcf09869574c968164b87
deenakamal <deenah.kamal@gmail.com>
2024-07-09 12:31:03 -0700

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' Module '''
+
+import asyncio
+import random
+
+async def async_generator() -> Generator[float, None, None]:
+    ''' Yield a random number between 0 and 10 '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
