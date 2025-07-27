2ee5c69d5289f8a9df63d66270acb5b6029c353b
deenakamal <deenah.kamal@gmail.com>
2025-07-27 17:10:55 +0300

    edit

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,6 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status, filters
 from rest_framework.response import Response
-from rest_framework.decorators import action
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -8,32 +7,29 @@ from .serializers import ConversationSerializer, MessageSerializer
 class ConversationViewSet(viewsets.ModelViewSet):
     """
     ViewSet for managing conversations.
-    Allows listing, retrieving, and creating conversations.
     """
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter] 
+    search_fields = ['participants__email']
 
     def perform_create(self, serializer):
-        """
-        When a new conversation is created, add the current user as a participant.
-        """
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     """
     ViewSet for managing messages.
-    Allows listing, retrieving, and sending messages to conversations.
     """
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['message_body']
 
     def perform_create(self, serializer):
-        """
-        When creating a message, set the sender as the current us
-        """
         serializer.save(sender=self.request.user)
-        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
